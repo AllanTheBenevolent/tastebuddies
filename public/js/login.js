8176d1966a2dba19c908fbba3ee3ff7dda022a1d
vagrant <vagrant@precise32.(none)>
2016-03-03 20:17:05 +0000

    testing login js

--- public/js/login.js
+++ public/js/login.js
@@ -27,6 +27,7 @@ $('.form').find('input, textarea').on('keyup blur focus', function (e) {
 
 });
 
+/*
 $('.tab a').on('click', function (e) {
   
   e.preventDefault();
@@ -42,5 +43,5 @@ $('.tab a').on('click', function (e) {
   $(target).fadeIn(600);
 
   
-});
+});*/
 
